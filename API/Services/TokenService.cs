7e456e5ea89b0aa963f19e0296d8f185e8774b7e
Deira Holidays <lipinnambiar@gmail.com>
2021-02-20 20:29:04 +0530

    sorting in api

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -24,7 +24,8 @@ namespace API.Services
             // creating new claims with username 
             var claims = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
             };
 
             // creating new security credentials using hmacsha512 signature
